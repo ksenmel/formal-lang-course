a6a694199c5fd61a7c475717955b0a4bc87f80ad
ksenmel <ksemelashenko@gmail.com>
2025-01-27 22:14:25 +0300

    add more visitor methods

--- project/interpreter/interpreter.py
+++ project/interpreter/interpreter.py
@@ -6,38 +6,38 @@ from project.parser.GQLVisitor import GQLVisitor
 from project.parser.task11_parser import program_to_tree
 
 
-class Memory:
+class Environment:
     def __init__(self):
-        self.tables = [{}]
+        self.env = [{}]
 
-    def add(self, name: str, value):
-        self.tables[-1][name] = value
+    def add(self, var: str, val):
+        self.env[-1][var] = val
 
     def next_scope(self):
-        new_table = Memory()
-        new_table.tables = self.tables.copy()
-        new_table.tables.append({})
-        return new_table
+        new_env = Environment()
+        new_env.env = self.env.copy()
+        new_env.env.append({})
+        return new_env
 
     def remove_last(self):
-        new_table = Memory()
-        new_table.tables = self.tables.copy()
-        new_table.tables = new_table.tables[:-1]
-        return new_table
+        new_env = Environment()
+        new_env.env = self.env.copy()
+        new_env.env = new_env.env[:-1]
+        return new_env
 
     def find(self, name: str):
-        scope_level = len(self.tables) - 1
+        scope_level = len(self.env) - 1
         while scope_level >= 0:
-            value = self.tables[scope_level].get(name)
+            value = self.env[scope_level].get(name)
             if value is not None:
                 return value
             scope_level -= 1
-        raise Exception("ti dolboeb")
+        raise Exception(f"Unknown variable: {name}")
 
 
 class MyVisitor(GQLVisitor):
     def __init__(self):
-        self.env = Memory()
+        self.env = Environment()
 
     def visitProg(self, ctx: ParserRuleContext):
         return self.visitChildren(ctx)
@@ -45,16 +45,32 @@ class MyVisitor(GQLVisitor):
     def visitStmt(self, ctx: GQLParser.StmtContext):
         return self.visitChildren(ctx)
 
-    def visitBind(self, ctx: GQLParser.BindContext):
-        var_name = ctx.var().getText()
-        value = self.visitChildren(ctx)
-        self.env.add(var_name, value)
-
     def visitExpr(self, ctx: GQLParser.ExprContext):
         return self.visitChildren(ctx)
 
+    def visitBind(self, ctx: GQLParser.BindContext):
+        var_name = ctx.var().getText()
+        value = self.visit(ctx.expr())
+        # value = self.visitExpr(ctx.expr())
+        self.env.add(var_name, value)
+
     def visitNum(self, ctx: GQLParser.NumContext):
         return int(ctx.NUM().getText())
+
+    def visitChar(self, ctx: GQLParser.CharContext):
+        return str(ctx.CHAR().getText()[1])
+
+    def visitVar(self, ctx: GQLParser.VarContext):
+        var_name = ctx.VAR().getText()
+        return self.env.find(var_name)
+
+    def visitSet_expr(self, ctx: GQLParser.Set_exprContext):
+        values = set()
+        for expr_ctx in ctx.expr():
+            value = self.visit(expr_ctx)
+            values.add(value)
+
+        return values
 
 
 def interpret(code: str):
@@ -67,5 +83,5 @@ def interpret(code: str):
 
 
 if __name__ == '__main__':
-    interp = interpret('let a = 15')
+    interp = interpret('let b = 1 let a = b let c = [1,2]')
     print(interp)
